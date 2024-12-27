47b5e50103c3a1522f077a9aac27f6a88efd831c
Georges034302 <georges.boughantous@gmail.com>
2024-12-27 22:19:04 +1100

    test 38

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -4,9 +4,9 @@ echo "======================"
 echo "Starting Frequency Analyzer..."
 
 # Running the Python frequency analyzer
-python3 /app/scripts/frequency.py /app/data.txt
+python3 /app/.github/scripts/frequency.py /app/data.txt
 
 # Running update_readme.sh
-bash /app/scripts/update_readme.sh
+bash /app/.github/scripts/update_readme.sh
 
 echo "Process Completed!"
