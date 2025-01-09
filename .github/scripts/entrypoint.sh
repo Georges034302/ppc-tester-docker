19eb080e9508b598a1ef869fef80597f760a1daf
Georges034302 <georges.boughantous@gmail.com>
2025-01-09 22:24:27 +1100

    update index added

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -5,10 +5,10 @@ echo "Starting Frequency Analyzer..."
 # Running the Python frequency analyzer
 FREQ_RESULT=$(python3 /app/.github/scripts/frequency.py /app/data.txt)
 
-# Running update_index.sh
-bash .github/scripts/update_index.sh "$FREQ_RESULT" "$GITHUB_USER"
-
 # Running update_readme.sh
+ bash .github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
+
+# Running update_index.sh
 if [ "$GITHUB_USER" == "Georges034302" ]; then
     bash .github/scripts/update_index.sh "$FREQ_RESULT" "$GITHUB_USER"
 fi
